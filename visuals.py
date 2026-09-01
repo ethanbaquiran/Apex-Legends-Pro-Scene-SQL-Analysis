a6ae7fad68a8e161fe420ac2a13573cc8e89c186
ethanbaquiran <ethanbaquiran12@gmail.com>
2026-08-31 18:19:09 -0700

    Finished up to

--- visuals.py
+++ visuals.py
@@ -15,7 +15,7 @@ ORDER BY total_earnings DESC
 LIMIT 10;
 """
 
-results = conn.execute(query).fetchall()
+results = conn.execute(query1).fetchall()
 players = [row[0] for row in results]
 earnings = [row[1] for row in results]
 
@@ -23,5 +23,48 @@ plt.figure(figsize=(12,6))
 plt.barh(players[::-1], earnings[::-1], color='skyblue')
 plt.xlabel('Total Earnings ($)')
 plt.title('Top 10 Highest Earning Apex Legends Pro Players')
+plt.tight_layout()
+plt.show()
+
+# Top Earning Orgs
+query2= """
+SELECT team, SUM(earnings) as total_earnings
+FROM org_winnings
+GROUP BY team
+ORDER BY total_earnings DESC
+LIMIT 10;
+"""
+
+results2 = conn.execute(query2).fetchall()
+orgs2 = [row[0] for row in results2]
+earnings2 = [row[1] for row in results2]
+
+plt.figure(figsize=(12,6))
+plt.barh(orgs2[::-1],earnings2[::-1],color='red')
+plt.xlabel('Total Earnings ($)')
+plt.title('Top 10 Highest Earning Orgs')
+plt.tight_layout()
+plt.show()
+
+# Annual Earnings between Orgs
+# cast(___ as integer) - turn values into integers
+query3= """
+SELECT cast(year as integer), SUM(earnings) as annual_earnings
+FROM org_winnings
+GROUP BY year
+ORDER by year DESC;
+"""
+
+results3 = conn.execute(query3).fetchall()
+year3 = [row[0] for row in results3]
+earnings3 = [row[1] for row in results3]
+
+plt.figure(figsize=(12,6))
+plt.plot(year3[::-1],earnings3[::-1])
+plt.xlabel('Year')
+plt.ylabel('Earnings ($)')
+plt.title('Annual Earnings')
+plt.xticks([2019,2020,2021,2022])
+plt.ticklabel_format(axis = 'y', style = 'plain')
 plt.tight_layout()
 plt.show()
